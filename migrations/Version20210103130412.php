f74b10559c6cc2fcf367a19f398fc3d149198013
Lorthioir <jeremy.lorthioir24@gmail.com>
2021-01-03 14:10:13 +0100

    [Fix] Rename Migrations files

--- migrations/Version20210103130412.php
+++ migrations/Version20210103130412.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210103130412_alter_loueur_station extends AbstractMigration
+final class Version20210103130412 extends AbstractMigration
 {
     public function getDescription() : string
     {
